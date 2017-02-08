20b524064f7fac24939fed40392a270ae3b548c7
casey.siebel <casey.siebel@baesystems.com>
2017-02-07 17:30:33 -0800

    added React components

---
+++ components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Video from './Video'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is the App Component</h1>
+        <Video/>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ components/Video.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TextDisplay extends Component {
+  render() {
+    return <div>I am a video</div>
+  }
+}
+
+//export default Vidoe
